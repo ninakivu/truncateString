b4f51ced9dadf12ddc2a30215825b32e38fdc2cf
Nina Klemencic <nina4klemencic@gmail.com>
2018-01-25 10:00:23 -0800

    Other versions

--- truncate.js
+++ truncate.js
@@ -6,5 +6,27 @@ function truncateString(str, len) {
     return new Error("First argument must be string, second argument must be number")
   }
 }
+console.log(truncateString("I have no idea if this works.", 6))
 
-truncateString("I have no idea if this works.", 3)
+
+
+// Noel's solution:
+function truncateString(str, len) {
+  var array = str.split(" ")
+  var result = ''
+  for(var i = 0; i < len; i++) {
+    result += array[i]
+  }
+  return result
+}
+
+// one liner:
+function truncateString(str, len) {
+  if(str.constructor === String && len.constructor === Number) {
+    return str.split('').splice(0, len).join('') + '...' 
+  } else {
+    return new Error("Argument types incorrect: str=String, len=Number")
+  }
+}
+console.log(truncateString("I have no idea if this works.", 6))
+console.log(truncateString(9, "Hello, world!!!"))
